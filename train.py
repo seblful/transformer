7baee3d5cf54708a9d739d6137df9d345309a5e8
seblful <alesha.chimba@gmail.com>
2024-01-22 11:55:09 +0300

    [ADD] start commit

---
+++ train.py
@@ -0,0 +1,9 @@
+from transformer import Transformer
+
+
+def main():
+    pass
+
+
+if __name__ == "__main__":
+    main
---
+++ transformer.py
@@ -0,0 +1,22 @@
+import torch
+import torch.nn as nn
+
+
+class Attention():
+    pass
+
+
+class MultiHeadAttention():
+    pass
+
+
+class TransformerEncoder():
+    pass
+
+
+class TransformerDecoder():
+    pass
+
+
+class Transformer():
+    pass
